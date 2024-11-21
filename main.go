9e2b2b3a403fd8423828b61cc3a9a37348ebf493
kenkn <skt7tp@gmail.com>
2024-11-21 12:00:38 +0900

    fixed

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
-import(
+import (
 	"fmt"
 	"net/http"
 	"os"
 )
 
-func main(){
+func main() {
 	err := http.ListenAndServe(
 		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter,r *http.Request){
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello,%s!", r.URL.Path[1:])
 		}),
 	)
-	if err != nil{
-		fmt.Fprintf("faild to terminate server: %v" , err)
-		os.Exit()
+	if err != nil {
+		fmt.Fprintln(os.Stdout, "Error: ", err)
+		os.Exit(1)
 	}
 }
