15ccdac6bc961fc0a93185660f2c1687c90d31be
nersan <narasuke114@icloud.com>
2024-11-25 20:30:21 +0900

    ver2

--- main.go
+++ main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
+	// "os"
 )
 
 func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello,%s!", r.URL.Path[1:])
+	// err := http.ListenAndServe(
+	// 	":18080",
+	// 	http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 		fmt.Fprintf(w, "Hello,%s!", r.URL.Path[1:])
+	// 	}),
+	// )
+	// if err != nil {
+	// 	fmt.Fprintln(os.Stdout, "Error: ", err)
+	// 	os.Exit(1)
+	// }
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
-	if err != nil {
-		fmt.Fprintln(os.Stdout, "Error: ", err)
-		os.Exit(1)
 	}
+	s.ListenAndServe()
 }
