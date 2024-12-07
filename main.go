b3e421f0c4f297887cc1e431f19b69ac835a0597
nersan <narasuke114@icloud.com>
2024-12-07 16:35:23 +0900

    run関数追加

--- main.go
+++ main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	// "os"
+
+	"golang.org/x/sync/errgroup"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Welcome to the Home Page!")
-}
-
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "This is the About Page!")
-}
-
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/about", aboutHandler)
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+	}
+}
 
-	fmt.Println("Starting server at :8080")
-	http.ListenAndServe(":18080", nil)
+func run(ctx context.Context) error{
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	//別ゴルーチンでHTTPサーバーを起動する。
+	eg.Go(func()error{
+		//http.ErrServerClosedは
+		//http.Server.Shutdown()が正常終了したことを示すので異常ではない。
+		if err := s.ListenAndServe(); err != nil &&
+		err != http.ErrServerClosed{
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+	//チャンネルからの通知（終了通知）を待機する
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	return eg.Wait()
 }
